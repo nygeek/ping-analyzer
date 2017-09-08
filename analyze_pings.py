bd8698826746e0bdbe85bc81dbdda36e2006e61d
Marc Donner <marc.donner@gmail.com>
2017-09-07 20:57:58 -0400

    clean up the classify code, eliminate globals, make handling cleaner

--- analyze_pings.py
+++ analyze_pings.py
@@ -7,46 +7,38 @@ from datetime import datetime
 import json
 import re
 
-linecount = 0
-down = 0
-route = 0
-timeout = 0
-normal = 0
-
 def classify(line):
     """ Given a line from the pinger output, classify it. """
     """ 64 bytes from 166.84.1.3: icmp_seq=19634 ttl=246 time=18.521 ms """
     """ ping: sendto: Network is down """
     """ ping: sendto: No route to host """
     """ Request timeout for icmp_seq 20043 """
-    global down
-    global route
-    global timeout
-    global normal
-    global linecount
 
-    linecount += 1
     if line == "ping: sendto: Network is down":
-        down += 1
+        return("Down")
     elif line == "ping: sendto: No route to host":
-        route += 1
+        return("Route")
     elif line.startswith("Request timeout for icmp_seq "):
-        timeout += 1
+        return("Timeout")
     else:
-        normal += 1
+        return("Normal")
 
 def main():
     """Main body."""
 
+    # Initialize the counters
+    counters = {"Down": 0, "Route": 0, "Timeout": 0, "Normal": 0}
+
+    linecount = 0
     for line in fileinput.input():
-        # print line.strip()
-        classify(line.strip())
+        linecount += 1
+        counters[classify(line.strip())] += 1
 
     print "linecount", linecount
-    print "down: ", down
-    print "route: ", route
-    print "timeout: ", timeout
-    print "normal: ", normal
+    print "down: ", counters["Down"]
+    print "route: ", counters["Route"]
+    print "timeout: ", counters["Timeout"]
+    print "normal: ", counters["Normal"]
 
 if __name__ == '__main__':
     main()
